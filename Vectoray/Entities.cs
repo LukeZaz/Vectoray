3f13bc7195ca9cf818371eb2eafcbcc9d01e0cf8
LukeZaz <luke@lyramg.com>
2019-01-26 04:40:27 -0600

    Added GetComponent and DestroyComponent, as well as cleaning up AddComponent to ensure entities only have one of a specific type of component.
    Also added some reference cleanup in Destroy functions to help prevent dead & uncollected entities/components from floating around.

--- Vectoray/Entities.cs
+++ Vectoray/Entities.cs
@@ -17,38 +17,88 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Vectoray.Entities
 {
 	public class Entity
 	{
-		public int id { get; private set; }
+		public const int NO_ENTITY_ID = -1;
+
+		/// <summary>
+		/// Unique identifier for this entity.
+		/// </summary>
+		public int id { get; private set; } = NO_ENTITY_ID;
+
+		/// <summary>
+		/// Components currently on this entity.
+		/// An entity cannot have more than one of a specific type of component.
+		/// </summary>
 		public List<Component> components { get; private set; } = new List<Component>();
 
+		// TODO: Constructor that accepts generic component types to add
+		/// <summary>
+		/// Create a new entity and automatically assign an ID to it.
+		/// </summary>
 		public Entity()
 		{
 			id = GetFreeId();
 			entities.Add(id, this);
 		}
 
+		/// <summary>
+		/// Destroy this entity and all of it's components.
+		/// </summary>
 		public void Destroy()
 		{
 			foreach (Component c in components) c.Destroy();
+			components.Clear();
+
 			FreeId(id);
+			id = NO_ENTITY_ID;
 		}
 
 		/// <summary>
-		/// Add a component to this entity.
+		/// Add a component of a given type to this entity.
 		/// </summary>
 		/// <typeparam name="T">Type of the component to add.</typeparam>
-		/// <param name="component">Type of the component to add.</param>
 		public void AddComponent<T>() where T : Component, new()
 		{
-			Component c = new T();
-			c.Initialize(this);
+			Component c = components.OfType<T>().FirstOrDefault();
+			if (c == null)
+			{
+				c = new T();
+				c.Initialize(this);
+			}
+			else Debugging.LogError($"AddComponent failed on Entity {id}: entities can only have one component of a type.");
 		}
 
-		internal void UpdateComponents(object caller)
+		/// <summary>
+		/// Get the component of this type from this entity.
+		/// </summary>
+		/// <typeparam name="T">The type of the component to get.</typeparam>
+		/// <returns>The component of the given type.</returns>
+		public Component GetComponent<T>() where T : Component => components.OfType<T>().FirstOrDefault();
+
+		/// <summary>
+		/// Remove the component of this type from the entity, if present.
+		/// </summary>
+		/// <typeparam name="T">The type of the component to remove.</typeparam>
+		public void DestroyComponent<T>() where T : Component
+		{
+			Component c = components.OfType<T>().FirstOrDefault();
+			if (c != null)
+			{
+				c.Destroy();
+				components.Remove(c);
+			}
+			else Debugging.LogError($"DestroyComponent failed on Entity {id}: this entity does not have a component of the provided type.");
+		}
+
+		/// <summary>
+		/// Update all components on this entity.
+		/// </summary>
+		internal void UpdateComponents()
 		{
 			foreach (Component c in components) c.Update();
 		}
@@ -108,14 +158,29 @@ namespace Vectoray.Entities
 	{
 		/// <summary>
 		/// Whether or not this component has been initialized yet.
+		/// This will be true if the internal Initialization function has been called (typically when an entity first adds this component to itself).
+		/// Will also become false when this components' Destroy function is called.
 		/// No regular functions (i.e. Update, Start, OnXYZ, etc) will run unless this is true.
 		/// </summary>
 		public bool initialized { get; private set; } = false; 
+
+		/// <summary>
+		/// Whether or not this entity is enabled.
+		/// Disabled entities do not run Update calls.
+		/// </summary>
 		public bool enabled { get; private set; } = true;
+
+		/// <summary>
+		/// The parent entity of this component.
+		/// </summary>
 		public Entity entity { get; private set; }
 
 		public Component() { }
 
+		/// <summary>
+		/// Initialize this component, setting it's parent and running it's Start function.
+		/// </summary>
+		/// <param name="parent">Parent entity for this component.</param>
 		internal void Initialize(Entity parent)
 		{
 			entity = parent;
@@ -146,14 +211,19 @@ namespace Vectoray.Entities
 
 		/// <summary>
 		/// Called once per frame.
+		/// Checks if this component is initialized and enabled and calls OnUpdate if so.
 		/// </summary>
 		public void Update()
 		{
-			if (initialized) OnUpdated();
+			if (initialized && enabled) OnUpdate();
+			else if (initialized) Debugging.LogWarning("'Update' called on disabled component!");
 			else Debugging.LogWarning("'Update' called on uninitialized component!");
 		}
 
-		protected virtual void OnUpdated() { }
+		/// <summary>
+		/// Called once per frame if this component is enabled and initialized.
+		/// </summary>
+		protected virtual void OnUpdate() { }
 
 		/// <summary>
 		/// Destroy this component.
@@ -161,7 +231,14 @@ namespace Vectoray.Entities
 		public void Destroy()
 		{
 			// TODO: determine if this needs to throw warnings out if uninitialized (probably not?)
-			if (initialized) OnDestroyed();
+			if (initialized)
+			{
+				OnDestroyed();
+				// Fully disable and reset this component to help ensure it gets handled by GC
+				entity = null;
+				enabled = false;
+				initialized = false;
+			}
 		}
 
 		/// <summary>
