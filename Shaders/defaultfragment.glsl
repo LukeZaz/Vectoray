6ea7d2ba4e96ede01f3c4a13c08656d4c3adee18
LukeZaz <LukeZaz@users.noreply.github.com>
2020-05-03 04:19:18 -0500

    Moved shaders folder, then added it as a content include for the .csproj file.

--- Shaders/defaultfragment.glsl
+++ Shaders/defaultfragment.glsl
@@ -1,6 +1,6 @@
 #version 460
 
-in VS_Output
+in VSOutput
 {
     vec3 color;
 } input;
--- Shaders/defaultvertex.glsl
+++ Shaders/defaultvertex.glsl
@@ -3,7 +3,7 @@
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 color;
 
-out VS_Output
+out VSOutput
 {
     vec3 color;
 } output;
